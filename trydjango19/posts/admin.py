5e51c944cac39e71f0d0f972ef98b8ff5b037c2c
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-03-31 00:55:29 +0530

    added timestamp

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__' , 'timestamp']
+    class meta:
+        model = Post
 
 
 
-admin.site.register(Post)
+admin.site.register(Post , PostModelAdmin)
