9c9b366a7fc94b016d87b731f4228200c204a405
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-03-31 01:07:02 +0530

    added list_display_links,list_filter,search_fields,list_editable

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['__unicode__' , 'timestamp']
+    list_display = ["title", "updated","timestamp"]
+    list_display_links = ["updated"]
+    list_filter = ["timestamp"]
+    search_fields = ["title","content"]
+    list_editable = ["title"]
     class meta:
         model = Post
 
