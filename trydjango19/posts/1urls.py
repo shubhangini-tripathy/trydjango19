85182d3cf93d2b8f0f2e10e8583f5c4b560bbdff
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-04-01 23:56:19 +0530

    template context

--- trydjango19/posts/1urls.py
+++ trydjango19/posts/1urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .import views
+from .views import(
+    post_create,
+    post_detail,
+    post_list,
+    post_update,
+    post_delete
+        
+
+)
 
 urlpatterns = [
-    url(r'^create/$', 'posts.views.post_create'),
-    url(r'^detail/$', 'posts.views.post_detail'),
-    url(r'^$', 'posts.views.post_list'),
-    url(r'^update/$', 'posts.views.post_update'),
-    url(r'^delete/$', 'posts.views.post_delete'),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^$', post_list),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete ),
 ]
--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -5,12 +5,25 @@ from django.shortcuts import render
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
+
+
 def post_detail(request):
-    return HttpResponse("<h1>detail</h1>")
+    context = {"title": "detail"}
+    return render(request, "index.html", context)
+
+
 def post_list(request):
-    return render(request,"index.html",{})
+    if request.user.is_authenticated():
+        context = {"title": "My User List"}
+    else:
+        context = {"title": "list"}
+    return render(request, "index.html", context)
+
+
     # return HttpResponse("<h1>list</h1>")
 def post_update(request):
     return HttpResponse("<h1>update</h1>")
+
+
 def post_delete(request):
     return HttpResponse("<h1>delete</h1>")
