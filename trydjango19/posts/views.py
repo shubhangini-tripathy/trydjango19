f1b0dc052c70113d4178a107cbc12080ac4d2869
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-04-02 00:58:51 +0530

    query sets

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+from .models import Post
+
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -13,11 +15,18 @@ def post_detail(request):
 
 
 def post_list(request):
-    if request.user.is_authenticated():
-        context = {"title": "My User List"}
-    else:
-        context = {"title": "list"}
+    queryset = Post.objects.all()
+    context = { 
+        "object_list": queryset,
+        "title": "list"
+    }
     return render(request, "index.html", context)
+
+    # if request.user.is_authenticated():
+    #     context = {"title": "My User List"}
+    # else:
+    #     context = {"title": "list"}
+    # return render(request, "index.html", context)
 
 
     # return HttpResponse("<h1>list</h1>")
