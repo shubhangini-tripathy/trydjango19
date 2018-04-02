f742b885d382f2dd612a6989489b1b23c0fc0dab
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-04-02 23:59:19 +0530

    used dynamic url routing

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 
 from .models import Post
@@ -9,9 +9,13 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):
-    context = {"title": "detail"}
-    return render(request, "index.html", context)
+def post_detail(request,id=None):
+    instance = get_object_or_404(Post,id = id)
+    context ={"title":instance.title,
+              "instance": instance,
+
+    }
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):
